0e8ed88386c13820b3d1191659a1337042978116
kadirisunday <kadirisunday212@gmail.com>
2025-03-18 19:02:22 +0100

    Added scrollable cards on hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_background from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 const Home = () => {
   return (
     <div className="home">
@@ -17,17 +18,18 @@ const Home = () => {
             Discovering his ties to secret ancient order, a young man living in
             modern Istabul embarks on a quest to save the city from immortal
             enemy.
-            <div className="hero-btns">
-              <button className="btn">
-                <img src={play_icon} alt="Play icon" />
-                Play
-              </button>
-              <button className="btn dark-btn">
-                <img src={info_icon} alt="Play icon" />
-                More Info
-              </button>
-            </div>
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="Play icon" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="Play icon" />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
         </div>
       </div>
     </div>
