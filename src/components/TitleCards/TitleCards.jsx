089f1342a200a6476e7d8877b6ec9a6c698de1b5
kadirisunday <kadirisunday212@gmail.com>
2025-03-19 04:45:19 +0100

    Added movies from IMDB API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,26 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMGU1OWQ1ZTkwYWVjYzBlOTUyZWUyYzY5NjMxMDgyZiIsIm5iZiI6MTc0MjM1MzE3Ni40MTkwMDAxLCJzdWIiOiI2N2RhMzMxODU5NGNhYzFlZTc2Y2EyOGYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.DVl7WzJW0V_0ojS4wE_WKTw5rLa67NNVcdd2ZtF5BoA",
+    },
+  };
+
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
 
   useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((movie, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt={card.name} />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + movie.backdrop_path}
+                alt={movie.backdrop_path}
+              />
+              <p>{movie.original_title}</p>
             </div>
           );
         })}
